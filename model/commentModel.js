8ae9d612c70d12419d3921bd50af3d9e5d2999c8
Emmyfrank <emmynono2013@gmail.com>
2024-02-18 17:13:24 +0200

    fixing comment error

--- model/commentModel.js
+++ model/commentModel.js
@@ -1,7 +1,19 @@
-const mongoose = require("mongoose");
-const commentSchema = new mongoose.Schema({
+// const mongoose = require("mongoose");
+// const commentSchema = new mongoose.Schema({
 
-    name: string,
-    comment: string,
+//     name: string,
+//     comment: string,
+// });
+// module.exports = mongoose.model("Comment", commentSchema);
+
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    name: String,
+    comment: String
 });
+
 module.exports = mongoose.model("Comment", commentSchema);
