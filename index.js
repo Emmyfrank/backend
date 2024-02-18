e58e3b54796f8da84cfc528cffe568836545b4a9
Emmyfrank <emmynono2013@gmail.com>
2024-02-18 16:22:52 +0200

    debug errors

--- index.js
+++ index.js
@@ -17,35 +17,50 @@ app.use(express.json());
 // Welcome route
 app.get("/", (req, res) => res.send("Welcome home"));
 
-// get all comments
-app.get("/api/comments", async (req, res)=>{
-  try {
-    const comments = await comment.find();
-    if(comments.length > 0){
-      res.status(200).json(comments);
-    }
-    else{
-      res.status(404).json({message: "no comments founds in database"})
-    }
-  } catch (error) {
-    res.status(500).json({message: error.message});
-  }
-});
+// // get all comments
+// app.get("/api/comments", async (req, res)=>{
+//   try {
+//     const comments = await comment.find();
+//     if(comments.length > 0){
+//       res.status(200).json(comments);
+//     }
+//     else{
+//       res.status(404).json({message: "no comments founds in database"})
+//     }
+//   } catch (error) {
+//     res.status(500).json({message: error.message});
+//   }
+// });
 
-// get single comment by Id
-app.get("/api/comments/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const comment = await User.findById(id);
-    if (comment) {
-      res.status(200).json({ message: "Comment found", comment });
-    } else {
-      res.status(404).json({ message: "Message not found or invalid user ID" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+// // get single comment by Id
+// app.get("/api/comments/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const comment = await Comment.findById(id);
+//     if (comment) {
+//       res.status(200).json({ message: "Comment found", comment });
+//     } else {
+//       res.status(404).json({ message: "Message not found or invalid user ID" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
+
+// // Delete comment by ID
+// app.delete("/api/comments/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const deletedComment = await Comment.findByIdAndDelete(id);
+//     if (deletedComment) {
+//       res.status(200).json({ message: "Comment found and successfully deleted", deletedUser });
+//     } else {
+//       res.status(404).json({ message: "Comment not found or invalid user ID and not deleted" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal server error" });
+//   }
+// });
 
 
 
