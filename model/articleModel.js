ee8aa1b64aadf56e918b83e4b2c2abbaac965563
Emmyfrank <emmynono2013@gmail.com>
2024-02-20 18:11:00 +0200

    fixed usera

--- model/articleModel.js
+++ model/articleModel.js
@@ -9,13 +9,3 @@ const articleSchema = new Schema({
 });
 
 module.exports = mongoose.model("Article", articleSchema);
-
-
-// const articleSchema = new mongoose.Schema({
-//   title: String,
-//   emage: String,
-//   discription: String,
-// });
-
-// module.exports = mongoose.model("Article", articleSchema);
-// >>>>>>> 1c5351904e79b44aacfc44b4358ba09dd4b60219
