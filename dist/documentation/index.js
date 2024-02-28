6298fc7446a1312a144b07314f33272b6fe7b70d
emmyfrank <emmynono2013@gmail.com>
2024-02-28 09:31:09 +0200

    unitial

--- dist/documentation/index.js
+++ dist/documentation/index.js
@@ -21,7 +21,7 @@ const options = {
             url: host,
         },
         {
-            url: "https://backend-mu-mauve.vercel.app/",
+            url: "https://backend-ctov.onrender.com/",
         },
     ],
     basePath: "/",
